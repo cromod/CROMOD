c4b228d055468b8b8ccd30cb5f5c38daa52bf263
cromod <cromod@users.noreply.github.com>
2015-08-23 02:20:09 +0200

    Move source files in inc/src directories + add CMakeLists.txt files

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.cpp
+ * Author: florent
+ *
+ * Created on 17 juin 2015, 23:50
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    return 0;
+}
