ec6d06ec31b417f6877551072128fd9abfede239
cromod <cromod@users.noreply.github.com>
2015-09-02 01:43:51 +0200

    Small correction

--- src/Polygon.cpp
+++ src/Polygon.cpp
@@ -49,7 +49,7 @@ Polygon::~Polygon()
 
 Segment& Polygon::operator[](const unsigned int& i)
 {
-    if (this->size()<0) return listSeg_[i];
+    if (this->size()>0) return listSeg_[i];
     else throw(Exception("Nothing to get with Polygon::operator[]"
                                  ,__FILE__,__LINE__)) ;
 }
