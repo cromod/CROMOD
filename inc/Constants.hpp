bc02d3fcadcf290fda60e27971223e07d13ac475
cromod <cromod@users.noreply.github.com>
2015-09-12 15:12:10 +0200

    Add doxygen doc to namespaces

--- inc/Constants.hpp
+++ inc/Constants.hpp
@@ -8,11 +8,14 @@
 #ifndef CONSTANTS_HPP
 #define	CONSTANTS_HPP
 
+/** @brief Cromod namespace */
 namespace Cromod {
+    /** @brief Geometric API */
     namespace GeomAPI {
         /** @brief Geometric tolerance */
         const double GEOM_TOLERANCE(1.e-6);
     }
+    /** @brief Field API */
     namespace FieldAPI {
         /** @brief Value tolerance */
         const double VAL_TOLERANCE(1.e-6);
