ab1c37fe5cb426108a6fcb445d77f3efc59ce4b0
cromod <cromod@users.noreply.github.com>
2015-09-19 19:57:47 +0200

    Add an option

--- config/MakeCoverage.cmake
+++ config/MakeCoverage.cmake
@@ -9,7 +9,7 @@ add_custom_target(coverage)
 add_custom_command (TARGET coverage
     COMMAND ${CMAKE_COMMAND} -E make_directory coverage
     #COMMAND ${LCOV_PATH} --directory . --zerocounters
-    COMMAND ${LCOV_PATH} --directory . --capture --output-file ./coverage/total.info
+    COMMAND ${LCOV_PATH} --directory . --capture --compat split_crc=auto --output-file ./coverage/total.info
     COMMAND ${LCOV_PATH} -r ./coverage/total.info '/usr/include/*' -o ./coverage/base.info
     COMMAND ${GENHTML_PATH} -o ./coverage ./coverage/base.info --legend
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR} )
